41f60f76283ebb540624fd8da05246bc1248a1d8
Ye-Ji-Kim <kyeoji0128@naver.com>
2019-11-24 20:55:48 +0900

    flask server

---
+++ register_check_test.py
@@ -0,0 +1,59 @@
+import cv2
+from PIL import Image
+import argparse
+from pathlib import Path
+from multiprocessing import Process, Pipe,Value,Array
+import torch
+from config import get_config
+from mtcnn import MTCNN
+from Learner import face_learner
+from utils import load_facebank, draw_box_name, prepare_facebank
+import sys
+from face_recognition import face_compare
+import requests
+import numpy as np
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='for face verification')
+    parser.add_argument("-s", "--save", help="whether save",action="store_true")
+    parser.add_argument('-th','--threshold',help='threshold to decide identical faces',default=1.54, type=float)
+    parser.add_argument("-u", "--update", help="whether perform update the facebank",action="store_true")
+    parser.add_argument("-tta", "--tta", help="whether test time augmentation",action="store_true")
+    parser.add_argument("-c", "--score", help="whether show the confidence score",action="store_true")
+    args = parser.parse_args()
+
+    conf = get_config(False)
+    server = "http://127.0.0.1:5000/"
+
+    mtcnn = MTCNN()
+    print('mtcnn loaded')
+
+    # inital camera
+    img = []
+    
+    img.append(cv2.imread('data/input/evans/evans_p.jpg'))
+    img.append(cv2.imread('data/input/hermsworth/hermsworth_p.jpg'))
+    img.append(cv2.imread('data/input/jeremy/jeremy.jpg'))
+    img.append(cv2.imread('data/input/mark/mark.jpg'))
+    img.append(cv2.imread('data/input/olsen/olsen.jpg'))
+    
+    faces=[]
+    
+    for i in range(5):
+    
+        image = Image.fromarray(img[i][...,::-1]) #bgr to rgb
+    #            image = Image.fromarray(img[i])
+        image = mtcnn.align(image)
+        print(image)
+        tolist_face = np.array(image).tolist()
+        faces.append(tolist_face) 
+            
+           
+    URL = server + "register_check"
+    json_feed = {'face_list': faces}
+    response = requests.post(URL, json=json_feed)
+    
+    print(response)
+    
+        
--- servertest.py
+++ servertest.py
@@ -2,8 +2,7 @@
 from flask import Flask, render_template, jsonify, request
 import os, sys, time
 # os.chdir(os.path.dirname(__file__))
-from PIL import Image
-from PIL import ImageTk
+from PIL import Image,ImageTk
 import json,cv2,argparse,torch
 import numpy as np
 from PIL import Image
@@ -15,49 +14,49 @@ from Learner import face_learner
 from utils import load_facebank, draw_box_name, prepare_facebank
 from datetime import datetime
 from mtcnn_pytorch.src.align_trans import get_reference_facial_points, warp_and_crop_face
-from face_recognition import get_face_feature
-import argparse
+from face_recognition import get_face_feature, get_max_cos #11.16
 
 app = Flask(__name__)
 
 server = "http://127.0.0.1:5000/"
-parser = argparse.ArgumentParser(description='for face verification')
-parser.add_argument('-th', '--threshold', help='threshold to decide identical faces', default=1.54, type=float)
-args = parser.parse_args()
-conf = get_config(False)
-
-register_list = []
+#11.16
+register_list=[] 
+conf = get_config(False) 
 learner = face_learner(conf, True)
 learner.threshold = args.threshold
+if conf.device.type == 'cpu':
+    learner.load_state(conf, 'cpu_final.pth', True, True)
+else:
+    learner.load_state(conf, 'final.pth', True, True)
+learner.model.eval()
 
 @app.route('/register',methods=["POST"])
 def register():
 
-    print("------------------------")
+    print("--------------")
 
     register_face = request.json['face_image']
-
     register_np = np.array(register_face)
-    #아래에 두 방법다 pil로 바껴유...
-
-    pil = Image.fromarray(register_np,'I;16')
-    #pil = Image.fromarray(register_np, 'RGB')
-
-    print(type(pil))
-    face_feature = get_face_feature(conf, learner.model, pil)
-    #register_list.append(face_feature)
+    
+    print(register_np)
 
     return "register success!"
 
-@app.route('/learn',methods=["POST"])
+@app.route('/register_check',methods=["POST"])
 def register_check():
     print(">>>>>>>>>>>>>")
-    face_name = request.json['name']
-    #learn_np = np.array(learn_score)
-    print(face_name)
 
-
-    return "register success!"
+    face_list = request.json['face_list']
+    check_list = []
+    for face in face_list:
+        feature = get_face_feature(conf, learner.model, face)
+        cos_sim = get_max_cos(feature, register_list)
+        if cos_sim > 0.7:
+            check_list.append("known")
+        else:
+            check_list.append("unknown")
+    
+    return check_list
 
 if __name__ =='__main__':
    app.run(host='0.0.0.0', port=5000,debug=True)
